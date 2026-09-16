81cbc982ef8d7f7a673c38e938d67bfd49c7b7f1
ElsayedAbdo219 <micro.elsayed219@gmail.com>
2026-09-17 00:15:43 +0300

    feat(chat):add chat and conversation migration.

--- database/migrations/2026_09_16_211437_create_chats_table.php
+++ database/migrations/2026_09_16_211437_create_chats_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('conversation_id')->references('id')->on('conversations')->after('id');
+            $table->enum('type', [
+                'text',
+                'image',
+                'file',
+                'voice',
+                'video',
+            ])->default('text')->after('message');
+
+            $table->foreignId('reply_to_id')
+                ->nullable()
+                ->constrained('chats')
+                ->nullOnDelete();
+             $table->boolean('is_edited')->default(false);
+             $table->timestamp('edited_at')->nullable();
+            $table->timestamp('deleted_for_everyone_at')->nullable();
+            $table->foreignId('sender_id')->references('id')->on('members'); 
+            $table->longText('message');
             $table->timestamps();
         });
     }
